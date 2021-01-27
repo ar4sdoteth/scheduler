535c45855b109a744a0fa96877a582d776638581
Ashley <ashley.barr@ar4s.com>
2021-01-27 14:12:48 -0700

    add test to mock axios

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -5,14 +5,3 @@ import { render, cleanup } from "@testing-library/react";
 import Appointment from '../Appointment/index';
 
 afterEach(cleanup);
-
-describe("Appointment Tests", () => {
-  it("renders without crashing", () => {
-    render(<Appointment />);
-  });
-  it("calls the function", () => {
-    const fn = jest.fn();
-    fn();
-    expect(fn).toHaveBeenCalledTimes(1);
-   });
-});
